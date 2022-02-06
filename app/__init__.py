8a7c9e50a09b4984432be74dc0765ca355585bec
Pavel Fedotov <66903336+Pfed-prog@users.noreply.github.com>
2022-02-06 03:22:53 +0300

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder="assets")
+CORS(app)
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 
 from flask import Flask, request, jsonify
+
 import requests
 import json
 import pandas as pd
